8a254725ce6e64813f1bf1b6789b83c6c9df6ba8
Elena Efimova <ees.elena@gmail.com>
2020-10-20 14:28:46 +0300

    [2-2] - fix trim and options

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,46 +1,52 @@
-'use strict';
-const {promises: fs} = require(`fs`);
-const path = require(`path`);
-const chalk = require(`chalk`);
+'use strict'
+const { promises: fs } = require(`fs`)
+const path = require(`path`)
+const chalk = require(`chalk`)
 
-const {getRandomInt, shuffle} = require(`../../utils`);
+const { getRandomInt, shuffle } = require(`../../utils`)
 
-const DEFAULT_COUNT = 1;
-const FILE_NAME = `mocks.json`;
+const DEFAULT_COUNT = 1
+const FILE_NAME = `mocks.json`
 
-const FILE_SENTENCES_PATH = `./data/sentences.txt`;
-const FILE_TITLES_PATH = `./data/titles.txt`;
-const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+const FILE_SENTENCES_PATH = `./data/sentences.txt`
+const FILE_TITLES_PATH = `./data/titles.txt`
+const FILE_CATEGORIES_PATH = `./data/categories.txt`
 
 const OfferType = {
   offer: `offer`,
   sale: `sale`,
-};
+}
 
 const SumRestrict = {
   min: 1000,
   max: 100000,
-};
+}
 
 const PictureRestrict = {
   min: 1,
   max: 16,
-};
+}
 
 const readContent = async (filePath) => {
   try {
-    const content = await fs.readFile(path.resolve(__dirname, filePath), `utf8`);
-    return content.split(`\n`);
+    const content = await fs.readFile(path.resolve(__dirname, filePath), `utf8`)
+
+    return content.split(`\n`).filter(contentString => {
+      contentString.trim();
+      return contentString.length > 0
+    })
   } catch (err) {
-    console.log(chalk.red(err));
-    return [];
+    console.log(chalk.red(err))
+    return []
   }
-};
+}
 
-const getPictureFileName = (number) => (number > 10 ? `item${number}.jpg` : `item0${number}.jpg`);
+const getPictureFileName = (number) => (number > 10 ? `item${number}.jpg` : `item0${number}.jpg`)
 
-const generateOffers = (count, titles, categories, sentences) =>
-  Array(count)
+const generateOffers = (count, options) => {
+  const { titles, categories, sentences } = options
+
+  return Array(count)
     .fill({})
     .map(() => ({
       category: shuffle(categories).slice(0, getRandomInt(1, categories.length - 1)),
@@ -49,24 +55,30 @@ const generateOffers = (count, titles, categories, sentences) =>
       title: titles[getRandomInt(1, titles.length - 1)],
       type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
       sum: getRandomInt(SumRestrict.min, SumRestrict.max),
-    }));
+    }))
+}
 
 module.exports = {
   name: `--generate`,
   async run(args) {
-    const sentences = await readContent(FILE_SENTENCES_PATH);
-    const titles = await readContent(FILE_TITLES_PATH);
-    const categories = await readContent(FILE_CATEGORIES_PATH);
+    const sentences = await readContent(FILE_SENTENCES_PATH)
+    const titles = await readContent(FILE_TITLES_PATH)
+    const categories = await readContent(FILE_CATEGORIES_PATH)
+    const options = {
+      titles,
+      categories,
+      sentences,
+    }
 
-    const [count] = args;
-    const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences));
+    const [count] = args
+    const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT
+    const content = JSON.stringify(generateOffers(countOffer, options))
 
     try {
-      await fs.writeFile(FILE_NAME, content);
-      console.log(chalk.green(`Operation success. File created.`));
+      await fs.writeFile(FILE_NAME, content)
+      console.log(chalk.green(`Operation success. File created.`))
     } catch (e) {
-      console.log(chalk.red(`Can't write data to file...`));
+      console.log(chalk.red(`Can't write data to file...`))
     }
   },
-};
+}
